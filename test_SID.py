5299f267cc13daf7e2bf4f43b2d295cb02d88972
neverlose <50455388+BuiBaAnh@users.noreply.github.com>
2024-01-15 15:20:18 +0700

    Update test_SID.py

--- test_SID.py
+++ test_SID.py
@@ -10,25 +10,25 @@ opt = BaseOptions().parse()
 
 cudnn.benchmark = True
 
-datadir = './datasets/SID/Sony'
+# datadir = './datasets/SID/Sony'
+datadir = '/kaggle/input/puwell-test/Puwell_Test'
 
 expo_ratio = [100, 250, 300] # [100, 250, 300] # , 100, 250
 read_expo_ratio = lambda x: float(x.split('_')[-1][:-5])
 
-train_fns = dataset.read_paired_fns('./dataset/Sony_train.txt')
-eval_fns = dataset.read_paired_fns('./dataset/Sony_val.txt')
-test_fns = dataset.read_paired_fns('./dataset/Sony_test.txt')
+# train_fns = dataset.read_paired_fns('./dataset/Sony_train.txt')
+# eval_fns = dataset.read_paired_fns('./dataset/Sony_val.txt')
+# test_fns = dataset.read_paired_fns('./dataset/Sony_test.txt')
 
-eval_fns_list = [[fn for fn in eval_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
-test_fns_list = [[fn for fn in test_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
-eval_fns_list = [lst_1 + lst_2 for lst_1, lst_2 in zip(eval_fns_list, test_fns_list)]
+# eval_fns_list = [[fn for fn in eval_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
+# test_fns_list = [[fn for fn in test_fns if min(int(read_expo_ratio(fn[1])/read_expo_ratio(fn[0])), 300)==ratio] for ratio in expo_ratio]
+# eval_fns_list = [lst_1 + lst_2 for lst_1, lst_2 in zip(eval_fns_list, test_fns_list)]
 
 # evaluate 15 indoor scenes (but you can also evaluate the performance on the whole dataset)
 indoor_ids = dataset.read_paired_fns('./SID_Sony_15_paired.txt')
 # indoor_ids = dataset.read_paired_fns('./SID_ablation.txt')
 # indoor_ids = dataset.read_paired_fns('./SID_Sony_paired.txt')
 eval_fns_list = [[(fn[0], fn[1]) for fn in indoor_ids if int(fn[2]) == ratio] for ratio in expo_ratio]
-
 noise_model = noise.NoiseModel(model="P+g", include=4)
 eval_datasets = [datasets.SIDDataset(datadir, fns, noise_model, memorize=False, size=None, augment=False, stage_in=opt.stage_in, stage_out=opt.stage_out) for fns in eval_fns_list]
 
